ac8d94ab982281b7489b06ae22df13d076850fd9
Dan Itsara <dan.itsara@gmail.com>
2023-11-05 17:25:35 +0700

    markdown rendering with auto-links for hash tags

--- app/rendering/redcarpet/custom_render.rb
+++ app/rendering/redcarpet/custom_render.rb
@@ -1,6 +1,11 @@
 class Redcarpet::CustomRender < Redcarpet::Render::HTML
+  include Rails.application.routes.url_helpers
+
+  # auto-links hash tags to an associated listing of Posts
   def postprocess(full_document)
-    Rails.logger.debug "custom render"
-    full_document
+    full_document.gsub HashTag::Scanner::MATCHER do |match|
+      param = HashTag.format(match)
+      "<a href='#{hash_tag_u_posts_path(param)}'>#{match}</a>"
+    end
   end
 end
