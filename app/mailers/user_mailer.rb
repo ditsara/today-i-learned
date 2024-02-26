85e2d26518665ee3146ed9fe536b01215a98fc83
Dan Itsara <dan.itsara@gmail.com>
2024-02-26 13:04:22 +0700

    reset password email

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,5 +1,7 @@
 class UserMailer < ApplicationMailer
   def reset_password_email(user)
-    # TODO
+    @user = User.find user.id
+    @url = edit_user_password_reset_url(@user.reset_password_token)
+    mail to: @user.email, subject: 'Your request to reset your password'
   end
 end
